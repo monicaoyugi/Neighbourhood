074296216e6bfac1ab28b99109f08f9f3972df7f
JKimani77 <jkkimani77@gmail.com>
2020-03-26 14:56:35 +0300

    created models and installed required dependencies

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+import datetime as DateTimeField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    '''
+    neighbourhood class and its methods
+    ''' 
+    neighbourhood_name = models.CharField(max_length=50)
+    location = models.CharField()
+
+    number_of_estates = models.CharField()
+
+class Business(models.Model):
+    '''
+    business class and its model
+    '''
+    business_name = models.CharField(max_length=50)
+    business_email = models.EmailField(null=True)
+    business_id = models.ForeignKey(Neighbourhood, blank = True, on_delete=models.CASCADE )
+
+class Profile(models.Model):
+    '''
+    normal user profile model and its methods
+    '''
+    profile_picture = models.ImageField(blank=True)
+    name_user = models.TextField(max_length=50, null=True, blank=True)
+    user = models.OneToOneField(User, blank=True, on_delete=models.CASCADE)
+
+class Posts(models.Model):
+    '''
+    model for alerts/meetings & posts
+    '''
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=80)
+    message = models.TextField(max_length=200, null=True, blank=True)
+    date_uploaded = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    estate = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
