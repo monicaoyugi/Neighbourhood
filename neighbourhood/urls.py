ef07a552bfbcc7c936e6bd6e6d89776a7d814f17
Murutu <murutupeter@email.com>
2020-03-26 21:39:24 +0300

    added  path add/business in urls.py

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path,re_path
 from .import views
 
 urlpatterns = [
     path('searchbusiness/', views.search_business, name="search_business"),
+    re_path(r'^business/(\d+)',views.single_business,name='singlebusiness'),
+    path('add/business',views.add_business,name='addbusiness')
     
 ]
