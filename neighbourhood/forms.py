525464ba149870078be09319f258abe4d599c240
Murutu <murutupeter@email.com>
2020-03-26 21:44:15 +0300

    added class BusinessForm in forms.py

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -1,3 +1,11 @@
 from django import forms
 from .models import Business
 
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['editor', 'area']
+        widgets={
+            'tags':forms.CheckboxSelectMultiple
+        }
+
