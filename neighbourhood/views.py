f3337a57492fe1e3a0df4cb3d06a7df8ba528f03
monicaoyugi <monicaoyugi@gmail.com>
2020-03-26 20:17:20 +0300

    added static folder

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    current_user = request.user.id
+    user = request.user
+    date = dt.date.today()
+    posts = Post.objects.all()
+    if Profile.objects.filter(user = request.user).count() == 0:
+        prof = Profile(user=request.user)
+        prof.save()
+    return render(request, 'index.html',{"date":date, "posts": posts})
