14f4bdd983a10199ce11bb6bcf755e56c8d91af5
Murutu <murutupeter@email.com>
2020-03-26 21:14:30 +0300

    added search_categories and a message in views.py

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -6,9 +6,9 @@ from .models import Business
 def search_results(request):
     if 'business' in request.GET and request.Get["business"]:
         search_term = request.GET.get("business")
-        searched_business = Business.search_business(search_term)
+        search_categories = Business.search_business(search_term)
         message = f"{search_term}"
-        return render(request, 'search.html',{"message":message,"business":searched_business})
+        return render(request,'searchbusiness.html',{"message":message,"business":searched_business})
     else:
-        message = "You haven't searched for any term"
+         message ="You haven't searched for any categories"
         return render(request,'search.html',{"message":message})
