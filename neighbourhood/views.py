3e9a74066a78417a5a7c9f50e3e8a623ea488246
Murutu <murutupeter@email.com>
2020-03-26 22:09:18 +0300

    added import redirect in views.py

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Business
 
 # Create your views here.
@@ -15,6 +15,20 @@ def search_results(request):
      
 def single_business(request,businessid):
     single_business=Business.single_business(businessid)
-    return render(request,'singlebusiness.html',{'singlebusiness':single_business})   
+    return render(request,'singlebusiness.html',{'singlebusiness':single_business}) 
+
+
+def addbusiness(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = addbusiness(request.POST,request.FILES)
+        if form.is_valid():
+            project =form.save(commit=False)
+            project.editor =current_user
+            project.save()
+            return redirect('home')
+    else:
+        form = NewProjectForm()
+    return render(request,'new_project.html',{"form":form})  
 
 
